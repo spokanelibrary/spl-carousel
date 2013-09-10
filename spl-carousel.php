5cd898fdd6997fe64263d736e86d78587c0fc32f
Sean Girard <seangirard@yahoo.com>
2013-09-10 12:35:39 -0700

    add the magic

---
+++ spl-carousel.php
@@ -0,0 +1,126 @@
+<?php
+
+/**
+ * @package SPL_Carousel
+ * @version 0.1
+ */
+
+/*
+Plugin Name: SPL Carousel
+Plugin URI: http://www.spokanelibrary.org/
+Description: Hooks the <code>[spl_carousel]</code> shortcode to show a <a href="http://twitter.github.io/bootstrap/javascript.html#carousel" target="_blank">Bootstrap Carousel</a>.
+Author: Sean Girard
+Author URI: http://seangirard.com
+Version: 0.1
+*/
+
+//add_filter( 'use_default_gallery_style', '__return_false' );
+
+function wp_spl_carousel($atts) {
+	
+	$id = get_the_ID();
+	$carousel = null;
+	$args = array(
+		'post_type' => 'attachment',
+		'orderby'		=> 'menu_order',
+		'order' 		=> 'ASC',
+		'numberposts' => null,
+		'post_status' => null,
+		'post_parent' => $id
+	); 
+	$attachments = get_posts($args);
+	
+	if ($attachments) {
+		$carousel .= ''.PHP_EOL;
+		$carousel .= '<div id="spl-carousel-'.$id.'" class="carousel slide">'.PHP_EOL;
+		
+		$i = 0;
+		$carousel .= '<ol class="carousel-indicators">'.PHP_EOL;	
+		foreach ($attachments as $attachment) {
+			$active = null;
+			if ( 0 == $i ) {
+				$active = ' class="active"';
+			} 
+			$carousel .= '<li data-target="#spl-carousel-'.$id.'" data-slide-to="'.$i.'"'.$active.'></li>'.PHP_EOL;
+			$i++;
+		}
+		$carousel .= '</ol>'.PHP_EOL;		
+
+		$carousel .= '<div class="carousel-inner">'.PHP_EOL;
+		
+		$i = 0;
+		foreach ($attachments as $attachment) {
+			
+			$alt = get_post_meta($attachment->ID, '_wp_attachment_image_alt', true);
+			
+			$active = null;
+			if ( 0 == $i ) {
+				$active = ' active';
+			} 
+
+			$carousel .= '<div class="item'.$active.'">'.PHP_EOL;
+
+			if ( !empty($alt) ) { 
+				$carousel .= '<a href="'.$alt.'">'.PHP_EOL;
+			}
+      $carousel .= '<img class="img-rounded" src="'.$attachment->guid.'" alt="'.$attachment->post_title.'">'.PHP_EOL;
+      if ( !empty($alt) ) { 
+				$carousel .= '</a>'.PHP_EOL;
+			}
+
+      $carousel .= '<div class="carousel-caption">'.PHP_EOL;
+
+      if ( !empty($alt) ) { 
+      	$carousel .= '<strong class="pull-right">'.PHP_EOL;
+      	$carousel .= '<a href="'.$alt.'">'.PHP_EOL;
+      	$carousel .= 'More &rarr;'.PHP_EOL;
+      	$carousel .= '</a>'.PHP_EOL;
+      	$carousel .= '</strong>'.PHP_EOL;
+      }
+
+
+      $carousel .= '<h4>';
+      $carousel .= $attachment->post_title;
+      if ( !empty($attachment->post_excerpt) ) {
+      	$carousel .= ' <small>'.$attachment->post_excerpt.'</small>';
+    	}
+    	$carousel .= '</h4>'.PHP_EOL;
+      
+      $carousel .= '<p>'.$attachment->post_content.'</p>'.PHP_EOL;
+
+      $carousel .= '</div>'.PHP_EOL;
+      $carousel .= '</div>'.PHP_EOL;
+
+			$i++;
+		}
+		
+		$carousel .= '</div>'.PHP_EOL;
+		
+  	$carousel .= '<a class="left carousel-control" href="#spl-carousel-'.$id.'" data-slide="prev"><span class="glyphicon glyphicon-circle-arrow-left"></span></a>'.PHP_EOL;
+ 	 	$carousel .= '<a class="right carousel-control" href="#spl-carousel-'.$id.'" data-slide="next"><span class="glyphicon glyphicon-circle-arrow-right"></span></a>'.PHP_EOL;
+		
+		$carousel .= '</div>'.PHP_EOL;
+		
+	}
+	
+	
+	foreach ($attachments as $attachment) {
+		/*
+		$carousel .= '<pre>'.PHP_EOL;
+		$carousel .= print_r($attachment, true) .PHP_EOL;
+		$carousel .= '</pre>'.PHP_EOL;
+		*/
+
+	}
+	
+
+	return $carousel;
+}
+
+//remove_shortcode( 'gallery', 'gallery_shortcode' ); /* Remove original shortcode */
+//add_shortcode( 'gallery', 'wp_spl_carousel' ); /* Add custom shortcode */
+
+add_shortcode('spl_carousel', 'wp_spl_carousel');
+
+
+?>
