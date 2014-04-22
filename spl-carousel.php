166692716b7ab7cf1976f120859422f53ae0bdb0
Sean Girard <seangirard@yahoo.com>
2014-04-22 11:39:51 -0700

    kiosk

--- spl-carousel.php
+++ spl-carousel.php
@@ -107,8 +107,10 @@ function wp_spl_carousel($atts) {
     
     $carousel .= '</div>'.PHP_EOL;
     
-    $carousel .= '<a class="left carousel-control" href="#spl-carousel-'.$id.'" data-slide="prev"><span class="glyphicon glyphicon-circle-arrow-left"></span></a>'.PHP_EOL;
-    $carousel .= '<a class="right carousel-control" href="#spl-carousel-'.$id.'" data-slide="next"><span class="glyphicon glyphicon-circle-arrow-right"></span></a>'.PHP_EOL;
+    if ( !in_array('kiosk', $atts) ) {
+      $carousel .= '<a class="left carousel-control" href="#spl-carousel-'.$id.'" data-slide="prev"><span class="glyphicon glyphicon-circle-arrow-left"></span></a>'.PHP_EOL;
+      $carousel .= '<a class="right carousel-control" href="#spl-carousel-'.$id.'" data-slide="next"><span class="glyphicon glyphicon-circle-arrow-right"></span></a>'.PHP_EOL;
+    }
     
     $carousel .= '</div>'.PHP_EOL;
     
