034bbfd07e51515cb5f0a5c8e4a62837e423a8b1
Sean Girard <seangirard@yahoo.com>
2015-01-15 12:06:43 -0800

    hero

--- spl-carousel.php
+++ spl-carousel.php
@@ -235,8 +235,8 @@ function wp_spl_carousel_hero($atts) {
 
       $carousel .= '<div class="item'.$active.'">'.PHP_EOL;
 
-      //$carousel .= '<div class="row">'.PHP_EOL;
-      //$carousel .= '<div class="col-md-6">'.PHP_EOL;
+      $carousel .= '<div class="row">'.PHP_EOL;
+      $carousel .= '<div class="col-md-6">'.PHP_EOL;
 
       if ( !empty($alt) ) { 
         $carousel .= '<a href="'.$alt.'">'.PHP_EOL;
@@ -246,8 +246,8 @@ function wp_spl_carousel_hero($atts) {
         $carousel .= '</a>'.PHP_EOL;
       }
 
-      //$carousel .= '</div>'.PHP_EOL; // col
-      //$carousel .= '<div class="col-md-6">'.PHP_EOL;
+      $carousel .= '</div>'.PHP_EOL; // col
+      $carousel .= '<div class="col-md-6">'.PHP_EOL;
 
       if ( !in_array('kiosk', $atts) ) {
         $carousel .= '<div class="carousel-caption">'.PHP_EOL;
@@ -271,8 +271,8 @@ function wp_spl_carousel_hero($atts) {
         $carousel .= '</div>'.PHP_EOL; // carousel-caption
       }
 
-      //$carousel .= '</div>'.PHP_EOL; // col
-      //$carousel .= '</div>'.PHP_EOL; // row
+      $carousel .= '</div>'.PHP_EOL; // col
+      $carousel .= '</div>'.PHP_EOL; // row
 
 
       $carousel .= '</div>'.PHP_EOL; // item?
