2ea47e1de478a641435e1e8c28f6672aa3473b33
Sean Girard <seangirard@yahoo.com>
2015-01-15 12:08:24 -0800

    hero

--- spl-carousel.php
+++ spl-carousel.php
@@ -237,7 +237,7 @@ function wp_spl_carousel_hero($atts) {
 
       $carousel .= '<div class="row">'.PHP_EOL;
       $carousel .= '<div class="col-md-6">'.PHP_EOL;
-      $carousel .= '<div>'.PHP_EOL;
+      $carousel .= '<div style="background:red;">'.PHP_EOL;
 
       if ( !empty($alt) ) { 
         $carousel .= '<a href="'.$alt.'">'.PHP_EOL;
