939434bf3ee47a3a6d416f2c1518697a5b1530b6
Sean Girard <seangirard@yahoo.com>
2015-01-15 12:31:10 -0800

    hero

--- spl-carousel.php
+++ spl-carousel.php
@@ -287,7 +287,7 @@ function wp_spl_carousel_hero($atts) {
     $carousel .= '</div>'.PHP_EOL; // carousel-inner
     
     if ( !in_array('kiosk', $atts) ) {
-      $carousel .= '<div class="style="width:50%">'.PHP_EOL;
+      $carousel .= '<div class="style=width:50%">'.PHP_EOL;
       $carousel .= '<a class="left carousel-control" href="#spl-carousel-'.$id.'" data-slide="prev"><span class="glyphicon glyphicon-circle-arrow-left"></span></a>'.PHP_EOL;
       $carousel .= '<a class="right carousel-control" href="#spl-carousel-'.$id.'" data-slide="next"><span class="glyphicon glyphicon-circle-arrow-right"></span></a>'.PHP_EOL;
       $carousel .= '</div>'.PHP_EOL;
