65d807b224bd3b174ffebb6af71ba759ea7d474b
Sean Girard <seangirard@yahoo.com>
2015-01-16 15:25:35 -0800

    hero

--- spl-carousel.php
+++ spl-carousel.php
@@ -242,9 +242,9 @@ function wp_spl_carousel_hero($atts) {
     
     $carousel .= '<div class="row">'.PHP_EOL;
     $carousel .= '<div class="col-md-12" style="z-index:6;"">'.PHP_EOL;
-    $carousel .= '<h2 class="text-primary" style="margin-top:3;">';
+    $carousel .= '<span class="text-primary" style="margin-top:0;">';
     $carousel .= 'New Year, New You, New Day for the Library';
-    $carousel .= '</h2>'.PHP_EOL;
+    $carousel .= '</span>'.PHP_EOL;
     $carousel .= '</div>'.PHP_EOL; // col
 
     $carousel .= '<div class="col-md-2">'.PHP_EOL;
