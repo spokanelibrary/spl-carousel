5c06fa7ed9a10d8d90cc4779f0a99e5841c34572
Sean Girard <seangirard@yahoo.com>
2015-01-28 14:02:51 -0800

    slides

--- class/SPL_Carousel.php
+++ class/SPL_Carousel.php
@@ -88,7 +88,7 @@ class SPL_Carousel {
     $carousel .= '<div style="width:100%;" id="spl-carousel-'.$this->id.'" class="carousel carousel-hero slide" '.$auto.' '.$interval.'>'.PHP_EOL;
     
     if ( !$this->kiosk ) {
-			$carousel .= '<div class="spl-blue" style="position:relative; height:50px; margin-top:0px; margin-bottom:16px;">'.PHP_EOL;
+			$carousel .= '<div class="spl-blue" style="position:relative; height:50px; margin-top:-4px; margin-bottom:16px;">'.PHP_EOL;
       $carousel .= '<a class="left carousel-control" href="#spl-carousel-'.$this->id.'" data-slide="prev"><span class="glyphicon glyphicon-circle-arrow-left"></span></a>'.PHP_EOL;
       $carousel .= '<a class="right carousel-control" href="#spl-carousel-'.$this->id.'" data-slide="next"><span class="glyphicon glyphicon-circle-arrow-right"></span></a>'.PHP_EOL;
 
