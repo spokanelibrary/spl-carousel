d7fc09ca0b09d368742d80cff1bfe499f0c33765
Sean Girard <seangirard@yahoo.com>
2015-01-29 12:37:04 -0800

    hero

--- class/SPL_Carousel.php
+++ class/SPL_Carousel.php
@@ -102,7 +102,7 @@ class SPL_Carousel {
     
     if ( !$this->kiosk ) {
 			$carousel .= '<div class="carousel-controls spl-hero-panel spl-hero-success">'.PHP_EOL;
-      $carousel .= '<h3 class="serif" style="margin-left:40px;">Read. Learn. Discover.</h3>'.PHP_EOL;
+      $carousel .= '<h3 class="hidden-xs" style="margin-left:40px;">Read. Learn. Discover.</h3>'.PHP_EOL;
       $carousel .= '<a class="left carousel-control" href="#spl-carousel-'.$this->id.'" data-slide="prev"><span class="glyphicon glyphicon-circle-arrow-left"></span></a>'.PHP_EOL;
       $carousel .= '<a class="right carousel-control" href="#spl-carousel-'.$this->id.'" data-slide="next"><span class="glyphicon glyphicon-circle-arrow-right"></span></a>'.PHP_EOL;
 
