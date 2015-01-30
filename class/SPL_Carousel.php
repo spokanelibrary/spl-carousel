d3f278b7c28add63ee04bb6d10c2f910925cc2db
Sean Girard <seangirard@yahoo.com>
2015-01-30 09:56:29 -0800

    hero

--- class/SPL_Carousel.php
+++ class/SPL_Carousel.php
@@ -403,7 +403,7 @@ class SPL_Carousel {
 	    //'numberposts' => $limit,
 	    'posts_per_page' => $limit,
 	    'category_name' => $category,
-	    'date_query' => array( 'column' => 'post_date', 'after' => strtotime('- 3 days'), 'before'=>'tomorrow' )
+	    'date_query' => array( 'column' => 'post_date', 'after' => '-3 days' )
 	  ); 
 	  
 	  $posts = new WP_query($args);
