53b22320152b04aca450c04296dff0b10ffbb0f6
Sean Girard <seangirard@yahoo.com>
2015-01-29 16:03:06 -0800

    hero

--- class/SPL_Carousel.php
+++ class/SPL_Carousel.php
@@ -290,9 +290,9 @@ class SPL_Carousel {
 		//$slide->subtitle = 'Subtitle';
 		$slide->content = '
 		<p class="lead">
-		<a href="http://news.spokanelibrary.org/new-year-new-you/">What’s on your “to do” list for 2015? <small class="text-muted">&rarr;</small></a></p>
+		<a href="http://news.spokanelibrary.org/new-year-new-you/">What’s on your “to do” list for 2015? <small class="text-muted">&rarr;</small></a>
     <br>
-    <a href="http://news.spokanelibrary.org/dewey_1-15/">Dewey’s (self) helpful side <small class="text-muted">&rarr;</small></a></p>
+    <a href="http://news.spokanelibrary.org/dewey_1-15/">Dewey’s (self) helpful side <small class="text-muted">&rarr;</small></a>
     <br>
     <a href="http://news.spokanelibrary.org/5_magazines_1-15/">Five Magazines instead of Five Songs This Month <small class="text-muted">&rarr;</small></a>
     </p>
