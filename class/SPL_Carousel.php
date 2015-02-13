006140db13be5f6f2268f9fd7007c12b1345e7b8
Sean Girard <seangirard@yahoo.com>
2015-02-13 13:45:55 -0800

    slides

--- class/SPL_Carousel.php
+++ class/SPL_Carousel.php
@@ -104,7 +104,7 @@ class SPL_Carousel {
 	  }
 
 	  if ( is_array($slides) ) {
-	  	$slides = array_slice($slides, 12);
+	  	//$slides = array_slice($slides, 12);
 	  	$this->slides = $slides;
 	  	$carousel = $this->getCarouselFormatted();
 	  }
