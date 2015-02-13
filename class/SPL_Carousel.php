f8f26a2b6fb626647d71148e340cf14392454f70
Sean Girard <seangirard@yahoo.com>
2015-02-13 12:52:33 -0800

    browse

--- class/SPL_Carousel.php
+++ class/SPL_Carousel.php
@@ -688,7 +688,7 @@ class SPL_Carousel {
 				if ( !empty($title->summary) && !empty($title->isbn) ) {
 					$slide = new stdClass();
 					$slide->format = 'browse';
-					$slide->title = $title->title;
+					$slide->title = $this->getCarouselExcerpt($title->title, 50, '');
 					$slide->author = $title->author;
 					$slide->img = 'http://contentcafe2.btol.com/ContentCafe/jacket.aspx?UserID=ebsco-test&Password=ebsco-test&Return=T&Type=M&Value='.$title->isbn;
 					$slide->url = '/bib/'.$title->bib.'/';
