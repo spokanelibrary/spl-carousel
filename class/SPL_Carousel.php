860815e7359a9cc69153bef8cda0fd1799095705
Sean Girard <seangirard@yahoo.com>
2015-03-03 11:03:07 -0800

    home

--- class/SPL_Carousel.php
+++ class/SPL_Carousel.php
@@ -152,7 +152,7 @@ class SPL_Carousel {
     
     // TOP NAV
     $skipnav = true;
-    if ( !$this->kiosk || $skipnav ) {
+    if ( !$this->kiosk || !$skipnav ) {
 			$carousel .= '<div class="carousel-controls spl-hero-panel spl-hero-success">'.PHP_EOL;
       if ( !empty($this->title) ) {
 	      $carousel .= '<div class="row">'.PHP_EOL;
