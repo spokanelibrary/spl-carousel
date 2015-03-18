24492ce41617f464eb8f9c02ff28c77d65ec0573
Sean Girard <seangirard@yahoo.com>
2015-03-18 10:03:33 -0700

    home

--- spl-carousel.php
+++ spl-carousel.php
@@ -247,7 +247,7 @@ function wp_spl_carousel_photo($atts) {
       }
 
       if ( !in_array('kiosk', $atts) ) {
-        $carousel .= '<div class="carousel-caption" style="background:#fff; width: 50%; position:absolute; bottom:0; right:0; margin-bottom:0;">'.PHP_EOL;
+        $carousel .= '<div class="carousel-caption" style="background:#fff; width: 50%; position:absolute; bottom:0; right:0 !important; margin-bottom:0;">'.PHP_EOL;
 
         if ( !empty($alt) ) { 
           $carousel .= '<a class="pull-right" href="'.$alt.'"> ';
