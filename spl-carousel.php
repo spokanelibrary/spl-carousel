aeda50d5096024ac3534ce6c45bc234ffc9a221e
Sean Girard <seangirard@yahoo.com>
2015-03-18 10:08:37 -0700

    home

--- spl-carousel.php
+++ spl-carousel.php
@@ -247,9 +247,9 @@ function wp_spl_carousel_photo($atts) {
       }
 
       if ( !in_array('kiosk', $atts) ) {
-        $carousel .= '<div style="background:#fff; position:absolute; bottom:0; right:0;">'.PHP_EOL;
         $carousel .= '<div class="row">'.PHP_EOL;
         $carousel .= '<div class="col-md-6">'.PHP_EOL;
+        $carousel .= '<div style="background:#fff; position:absolute; bottom:0; right:0;">'.PHP_EOL;
         $carousel .= '<div class="carousel-caption">'.PHP_EOL;
 
         if ( !empty($alt) ) { 
